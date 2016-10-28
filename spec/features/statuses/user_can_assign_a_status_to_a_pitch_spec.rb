47e9ed55e1d7df448926a58e72a16ce624ff015d
Noah Berman <noah@noahberman.org>
2016-10-28 11:11:48 -0600

    user can edit a status on an existing pitch

--- spec/features/statuses/user_can_assign_a_status_to_a_pitch_spec.rb
+++ spec/features/statuses/user_can_assign_a_status_to_a_pitch_spec.rb
@@ -27,4 +27,36 @@ RSpec.describe "User can assign a status to a pitch" do
     expect(page).to have_content("Old")
     
   end
+  
+  scenario "a user can edit an existing pitch" do
+    user = Fabricate(:user)
+    status = Fabricate(:status, title: "New")
+    pitch = Fabricate(:pitch, user: user, status: status)
+    status_2 = Fabricate(:status, title: "Old")
+    
+    visit root_path
+    click_link "Login"
+    
+    fill_in "email", with: "#{user.email}"
+    fill_in "password", with: "#{user.password}"
+
+    click_button "Submit"
+    
+    click_link "#{user.name}"
+    
+    expect(page).to have_content("New")
+    
+    click_link "#{pitch.title}"
+    
+    expect(page).to have_content("Edit Pitch")
+    
+    click_link "Edit Pitch"
+
+    find(:xpath, '//option[contains(text(), "Old")]').select_option
+    
+    click_button "Update Pitch"
+    
+    expect(page).to have_content("#{pitch.title}")
+    expect(page).to have_content("Old")
+  end
 end
